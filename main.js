d982325c0c099b235a7801b244669dcf6a4b520d
abrangel <84999712+abrangel@users.noreply.github.com>
2025-10-24 21:12:22 -0500

    Implement translucent and hideable window

--- main.js
+++ main.js
@@ -1,16 +1,27 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, globalShortcut } = require('electron');
 const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    frame: false,
+    transparent: true,
+    alwaysOnTop: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   });
 
   win.loadFile('index.html');
+
+  globalShortcut.register('CommandOrControl+Shift+H', () => {
+    if (win.isVisible()) {
+      win.hide();
+    } else {
+      win.show();
+    }
+  });
 }
 
 app.whenReady().then(() => {
